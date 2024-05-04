3d55e5d45e2467595928e857dd82e123d3a672bf
lquiyuch2022226 <lquiyuch-2022226@kinal.edu.gt>
2024-05-03 19:10:32 -0600

    Se empieza con las rutas para habitaciones y se crean validaciones para habitaciones

--- src/helpers/db-validator.js
+++ src/helpers/db-validator.js
@@ -1,4 +1,5 @@
 import User from "../user/user.model.js"
+import Room from "../room/room.model.js"
 
 export const existeEmail = async (email = '') => {
     const existe = await User.findOne({email})
@@ -11,5 +12,12 @@ export const existeUsuarioById = async (id = '') =>{
     const existeUsuario = await User.findById(id)
     if(!existeUsuario){
         throw new Error(` el ID: ${id} no existe` )
+    }
+}
+
+export const existRoomById = async (id = '') =>{
+    const existRoom = await Room.findById(id)
+    if(!existRoom){
+        throw new Error(`The room with this id: ${id} don't exist`)
     }
 }
---
+++ src/room/room.routes.js
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+import { check } from 'express-validator';
+import { validarCampos } from '../middlewares/validar-campos.js';
+import { validarJWT } from '../middlewares/validar-jwt.js';
+import { existRoom } from '../helpers/db-validators.js';
