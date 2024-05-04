d6b190a8bebe1eea737eec0b26adf5a1724f113b
lquiyuch2022226 <lquiyuch-2022226@kinal.edu.gt>
2024-05-03 19:14:27 -0600

    Se terminan las rutas para habitaciones

--- src/room/room.routes.js
+++ src/room/room.routes.js
@@ -2,4 +2,58 @@ import { Router } from 'express';
 import { check } from 'express-validator';
 import { validarCampos } from '../middlewares/validar-campos.js';
 import { validarJWT } from '../middlewares/validar-jwt.js';
-import { existRoom } from '../helpers/db-validators.js';
+import { existRoomById } from '../helpers/db-validators.js';
+
+import {
+    roomPost,
+    roomsGet,
+    roomPut,
+    roomDelete,
+    roomGetById
+} from '../room/room.controller.js';
+
+const router = Router();
+
+router.post(
+    "/",
+    [
+        validarJWT,
+        check('type', 'The type of the room is required').not().isEmpty(),
+        check('capacity', 'The capacity of the room is required').not().isEmpty(),
+        check('price', 'The price of the room is required').not().isEmpty(),
+        check('availableDate', 'The available date of the room is required').not().isEmpty(),
+        check('idHotel', 'The Id Hotel of the room is required').not().isEmpty(),
+        check('idUser', 'The idUser of the room is required').not().isEmpty(),
+        validarCampos
+    ], roomPost
+);
+
+router.get("/",
+    validarJWT,
+    roomsGet);
+
+router.get(
+    "/:id",
+    [
+        validarJWT,
+    ],
+    roomGetById
+);
+
+router.put(
+    "/:id",
+    [
+        validarJWT,
+        validarCampos
+    ], roomPut
+);
+
+router.delete(
+    "/:id",
+    [
+        validarJWT,
+        validarCampos
+    ], roomDelete
+);
+
+export default router;
